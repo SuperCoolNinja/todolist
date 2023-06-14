0f77153ed047b3310fe25fb6550956e801cd5e07
SuperCoolNinja <sbpro570@gmail.com>
2023-06-14 12:03:56 +0200

    cleanup notificaiton need to set the notification as component when got time

--- src/components/cards/card-left.tsx
+++ src/components/cards/card-left.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ProgressBar } from "../progress/index.tsx";
 import { TaskContext } from "../cards/index.tsx";
 import { Tasks } from "../Tasks/index.tsx";
@@ -6,40 +6,21 @@ import { Tasks } from "../Tasks/index.tsx";
 import style from "./style.module.scss";
 
 export const CardLeft: React.FunctionComponent = () => {
-  const {tasks, setTask } = useContext(TaskContext);
+  const { tasks, setTask } = useContext(TaskContext);
   const [value, setValue] = useState<string>("");
   const [totalTasksCount, setTotalTasksCount] = useState<number>(0);
   const [completedTasksCount, setCompletedTasksCount] = useState<number>(0);
   const [showNotification, setShowNotification] = useState<boolean>(false);
+  const [txtNotification, setTxtNotification] = useState<string>("");
 
   useEffect(() => {
     setTotalTasksCount(tasks.length);
     setCompletedTasksCount(tasks.filter((task) => task.isFinish).length);
   }, [tasks]);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (value !== "") {
-      const inputCopy = [
-        ...tasks,
-        { id: tasks.length, task: value, isFinish: false },
-      ];
-      setTask(inputCopy);
-      setValue("");
-    }
-  };
-
-  const isAllTasksCompleted = completedTasksCount === totalTasksCount;
-
-  const onDelete = (id: number) => {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    setTask(updatedTasks);
-  };
-
   useEffect(() => {
-    if (isAllTasksCompleted) {
-      setCompletedTasksCount(totalTasksCount);
+    if (completedTasksCount === totalTasksCount && totalTasksCount > 0) {
+      setTxtNotification("All tasks completed! Congratulations!");
       setShowNotification(true);
 
       const timeout = setTimeout(() => {
@@ -48,14 +29,40 @@ export const CardLeft: React.FunctionComponent = () => {
       }, 2000);
 
       return () => clearTimeout(timeout);
-    } else {
-      setShowNotification(false);
     }
-  }, [isAllTasksCompleted, setTask, totalTasksCount]);
+  }, [completedTasksCount, totalTasksCount, setTask]);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (value.trim() !== "") {
+      const newTask = {
+        id: tasks.length + 1,
+        task: value,
+        isFinish: false,
+      };
+      setTask([...tasks, newTask]);
+      setValue("");
+    }
+  };
+
+  const onDelete = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTask(updatedTasks);
+    setCompletedTasksCount(completedTasksCount - 1); // Mise à jour du compteur de tâches terminées
+    setTxtNotification("Task deleted !");
+    setShowNotification(true);
+
+    const timeout = setTimeout(() => {
+      setShowNotification(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  };
 
   return (
     <div className={style.card_left}>
-      <form onSubmit={(e) => onSubmit(e)}>
+      <form onSubmit={onSubmit}>
         <input
           className={style.input}
           type="text"
@@ -72,9 +79,9 @@ export const CardLeft: React.FunctionComponent = () => {
         <ProgressBar value={completedTasksCount} max={totalTasksCount} />
       )}
 
-      {totalTasksCount > 0 && showNotification && (
-        <div className={style.notification}>
-          All tasks completed! Congratulations!
+      {showNotification && (
+        <div className={`${style.notification} ${style.success}`}>
+          {txtNotification}
         </div>
       )}
     </div>
